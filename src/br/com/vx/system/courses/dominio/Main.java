f3818f90505681516af4a7f4ddb655a6e21b563c
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2023-07-05 08:27:45 -0300

    Interligando as classes de dominio

--- src/br/com/vx/system/courses/dominio/Main.java
+++ src/br/com/vx/system/courses/dominio/Main.java
@@ -30,10 +30,51 @@ public class Main {
 		mentoria1.setDescricao("Descrição mentoria java");
 		mentoria1.setData(LocalDate.now());
 		
-		System.out.println(curso1);
+	 /*	System.out.println(curso1);
 		System.out.println(curso2);
-		System.out.println(mentoria1);
-
+		System.out.println(mentoria1);*/
+		
+		// CRIANDO O BOOTCAMP
+		Bootcamp bootcamp = new Bootcamp();
+		bootcamp.setNome("Bootcamp Java Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+		bootcamp.getConteudos().add(curso1);
+		bootcamp.getConteudos().add(curso2);
+		bootcamp.getConteudos().add(mentoria1);
+		
+		
+		// CRIANDO O DEV
+		Dev devVini = new Dev();
+		devVini.setNome("Vinicius");
+		devVini.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos " + devVini.getConteudoInscritos());
+		
+		// quando eu concluir um curso, eles automaticamente serão removidos de conteudosInscritos
+		devVini.progredir();
+		devVini.progredir();
+		System.out.println("-");
+		System.out.println("Conteudos Inscritos " + devVini.getConteudoInscritos());
+		System.out.println("Conteudos Concluidos " + devVini.getConteudosConcluidos());
+		System.out.println("XP: " + devVini.calcularTotalXp());
+		System.out.println();
+		System.out.println("-----------------------------------");
+		System.out.println();
+		
+		Dev devJoao = new Dev();
+		devJoao.setNome("João");
+		devJoao.inscreverBootcamp(bootcamp);
+		System.out.println("Conteudos Inscritos " + devJoao.getConteudoInscritos());
+		
+		
+		devJoao.progredir();
+		devJoao.progredir();
+		devJoao.progredir();
+		/*Se progredir mais que a quantidade de conteudo será exibido um erro*/
+	//	devJoao.progredir();
+		System.out.println("-");
+		System.out.println("Conteudos Inscritoa -> " + devJoao.getConteudoInscritos());
+		System.out.println("Conteudos Concluidos " + devJoao.getConteudosConcluidos());
+		System.out.println("XP: " + devJoao.calcularTotalXp());
 	}
 
 }
